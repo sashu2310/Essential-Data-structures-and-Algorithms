741ea0d890148e3e0ea185a8d02980270bf60c8f
sandeep kesarwani <sandeepkesar2310@gmail.com>
2018-07-24 12:32:29 +0530

    Create stack.c
    Stack implementation in C

---
+++ Data_Structures/stack.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#define MAX 100001;                 // Max size of stack
+int stack [MAX];
+int top=-1;                         // current stack is empty
+
+void push(int data){                //function to push elements
+    if(top==MAX) {
+        printf("Overflow");
+        return; }
+    top++;                          //incrementing size
+    stack[top] = data;              // storing data
+}
+
+int pop() {                         //function to remove elements from top
+    int data;
+    if(top==-1) {                   // When stack is empty
+        printf("stack empty");
+        return;
+    }
+    data = stack[top];               // extracting top element
+    top--;                           //decrementing size
+    return data;
+
+}
+ int main() {
+   int choice,value;
+   printf("Options: 1. Push 2. Pop\n");
+   scanf("%d",&choice);
+   if(choice==1)
+    {
+        printf("Enter value");
+        scanf("%d",&value);
+        push(value);
+    }
+    else if(choice==2)
+        printf(pop());
+    else
+        printf("Wrong option");
+    return 0;
+ }
