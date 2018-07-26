c1af6dc2d1317dd6961e8b0c373a9b71ac93574b
sandeep kesarwani <sandeepkesar2310@gmail.com>
2018-07-26 12:37:46 +0530

    Update Linked_List.c

--- Data_Structures/Linked_List.c
+++ Data_Structures/Linked_List.c
@@ -2,76 +2,92 @@
 
 struct node {
     int data;
-    struct link_list *next;
+    struct node *next;
 };
-typedef struct node snode;      // creating new alias as a type for struct node
-snode *head = NULL;
 
-snode* create_node(){           //creating node through dynamic memory allocation
-    snode *temp;
-    temp = (snode*) malloc(sizeof(snode));  //malloc() function returns the address of the memory location from heap section
-    temp->next = NULL;
-    return temp;
-}
+// creating new alias as a type for struct node      
+struct node *head = NULL;
+
+// snode* create_node(){           //creating node through dynamic memory allocation
+//     snode *temp;
+//     temp = (snode*) malloc(sizeof(snode));  //malloc() function returns the address of the memory location from heap section
+//     temp->next = NULL;
+//     return temp;
+// }
 
 int list_length() {
-	int length=0;
-	node *temp = head;
-	while(temp!=Null) {
-		length++;
-		temp=temp->next; }
-	return length;
-		
+    struct node *temp = head;
+    int length=0;
+    while(temp!=NULL)
+        length++;
+    return length;
 }
 
 void insert_at_tail(int data) {     //inserting node at the end of the linked list
-    snode *temp, *p;
-    temp = create_node();
+    struct node *temp, *current;
+    temp = (struct node*) malloc(sizeof(struct node));
     temp->data = data;
-    if(head == NULL) {
+    temp->next = NULL;
+    if(head == NULL) 
         head = temp;
-    }
     else {
-        p = head;
-        while(p!=NULL) {
-         p = p->next; }
-         p->next = NULL;
+        current = head;
+        while(current!=NULL) {
+         current = current->next; }
+         current->next = temp;
     }
 }
-
-void insert_at_head(int data)		// Insert the newly created node at the beginning of the list
+void insert_at_head(int data)
 {
-    snode *temp, *p;
-    temp = create_node();
-    temp->data=data;
+    struct node *temp, *p;
+    temp = (struct node*) malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = NULL;
     if(head==NULL) {
         head = temp;
     }
     else
     {
-        p=head;
-        temp->next=p;
-        head=temp;
+        p = head;
+        temp->next = p;
+        head = temp;
     }
 }
-
-void insert_at_position(int data,int position) {	//Insert the newly created node at any specified position 
-    int length;
-    node *temp,*p;
+void insert_at_position(int data,int position) {
+    struct node *temp,*p;
+    //considering 1 as the first position 
+    if(list_length()<position)  {
+        printf("Not enough long");
+        return;
+    }
+    //if position is at the end
+    else if(list_length()==position)
+        insert_at_tail(data);
+    // if list is empty yet
+    else if(position==1)
+        insert_at_head(data);
+    else {
     temp=create_node();
     temp->data = data;
     p = head;
-    if(length = list_length()<position-1)    	//checking whether list is long enough
-	    printf("Position doesn't exists");
-    while(temp!=NULL || i<position) {
+    position--;
+    // drive to the one node before the position
+    while(position--) {
         p = p->next;
     }
+    //link new node to it's neighbour
     temp->next = p->next;
+    //link previous node to new node
     p->next = temp;
+    }
+}
+
+void delete_at_tail() {
+    
 }
 
 void print(){                   //printing all the members of the linked list
-    snode *temp = head;
+    struct node *temp = head;
     while(temp!=NULL) {
         printf("%d\n", temp->data);
         temp = temp->next;
