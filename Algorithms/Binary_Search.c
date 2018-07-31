162bebb73e98b01a51c50db937da103c07156b5f
sandeep kesarwani <sandeepkesar2310@gmail.com>
2018-07-31 17:27:55 +0530

    Update Binary_Search.c

--- Algorithms/Binary_Search.c
+++ Algorithms/Binary_Search.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 // Array to be searche in
-int A[10001];
 //function implementing binary search algorithm 
-int binarySearch(int n,int key) {
+int binarySearch(int n,int key,int *A) {
     int start=0,end=n-1,mid;
     while(start < end) {
-        mid = start+end/2;
+        mid = start+(end-1)/2;
         //key found
         if(A[mid]==key)
             return mid;
@@ -19,13 +18,13 @@ int binarySearch(int n,int key) {
 return -1;
 }
 int main() {
-    int n,i,key;
+    int n,i,key,A[10001];;
     scanf("%d",&n);
     for(i=0;i<n;i++)
         scanf("%d",&A[i]);
     printf("Enter number to be searched\n");
     scanf("%d",&key);
     //suppose array to be sorted
-    printf("%d",binarySearch(n,key));
+    printf("%d",binarySearch(n,key,&A[0]));
     return 0;
 }
