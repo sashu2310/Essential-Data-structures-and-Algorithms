24acdf5fe1d8df968cb54490ca734fb94d14923d
sandeep kesarwani <sandeepkesar2310@gmail.com>
2018-08-02 12:57:24 +0530

    Update upperBound.c

--- Algorithms/Searching/upperBound.c
+++ Algorithms/Searching/upperBound.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int lowerBound(int n,int key,int *A) {
+int upperBound(int n,int key,int *A) {
     int start=0,end=n-1,mid,ans;
     while(start < end) {
         mid = start+end/2;
@@ -8,14 +8,15 @@ int lowerBound(int n,int key,int *A) {
             ans = mid;
             end = mid-1;
         }
-        //key is small check the left half
+        //key is small check the right half
         else if(A[mid]<=key)
             start = mid+1;
     }
 return ans;
 }
+
 int main() {
-int A[9] = {0,2,3,4,5,5,6,7,8};
-printf("%d",lowerBound(9,5,&A[0]));
+int A[9] = {0,2,3,4,5,5,7,8,9};
+printf("%d",upperBound(9,5,&A[0]));
 return 0;
 }
